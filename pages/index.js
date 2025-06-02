4c3efeed5084e80271c251a7197b849f8212b44e
saddamonpc <saddamonpc@gmail.com>
2025-06-02 11:47:35 +0700

    Update locations db and categories

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import Date from '../components/date';
 
 export default function Home({ allPostsData }) {
   return (
